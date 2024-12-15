eb471d539b38644d47f3567554e93e8c7c83de58
NStoyanov95 <nstoyanov9512@gmail.com>
2024-12-15 19:19:06 +0200

    Add Card component to CardContainer for dynamic data display

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import styles from "./CardContainer.module.css";
+import Card from "../Card/Card";
 
 const CardContainer = () => {
     const [items, setItems] = useState([]);
@@ -11,28 +12,16 @@ const CardContainer = () => {
                 "https://api.escuelajs.co/api/v1/products"
             );
             const data = await response.json();
-
             setItems(data);
         })();
     }, []);
 
-    console.log(items);
-
     return (
         <>
-            <h1>Card Container</h1>
             <section className={styles.cardContainer}>
-                <div className={styles.card}>
-                    <div className={styles.cardContent}>
-                        <img src="https://i.imgur.com/qNOjJje.jpeg" alt="img" />
-                        <h3>Cloth</h3>
-                        <p className={styles.description}>Category: Clothes</p>
-                        <div className={styles.cardInfo}>
-                            <p>Price: 200$</p>
-                            <button>Details</button>
-                        </div>
-                    </div>
-                </div>
+                {items.map((item) => (
+                    <Card item={item} key={item.id} />
+                ))}
             </section>
         </>
     );
