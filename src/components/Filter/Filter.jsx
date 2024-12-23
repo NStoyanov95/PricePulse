e4ed860c53ae9b023ba743709b1e61f98c0bcf8b
NStoyanov95 <nstoyanov9512@gmail.com>
2024-12-23 21:12:43 +0200

    Edit filter styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,17 @@
+import styles from "./Filter.module.css";
+
 const Filter = ({ filter, setFilterHandler }) => {
     return (
-        <select
-            value={filter}
-            onChange={(e) => setFilterHandler(e.target.value)}
-        >
-            <option value="defaultFilter">Default</option>
-            <option value="cloth">Clothes</option>
-            <option value="electronics">Electronics</option>
-        </select>
+        <div className={styles.filterContainer}>
+            <select
+                value={filter}
+                onChange={(e) => setFilterHandler(e.target.value)}
+            >
+                <option value="defaultFilter">Default</option>
+                <option value="cloth">Clothes</option>
+                <option value="electronics">Electronics</option>
+            </select>
+        </div>
     );
 };
 
