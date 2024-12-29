66d4974c32a12112dc0ce3fb01648d736069252a
NStoyanov95 <nstoyanov9512@gmail.com>
2024-12-29 10:30:15 +0200

    Create homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,6 +16,33 @@ const Home = () => {
                     </div>
                     <button className={styles.catalogBtn}>Catalog</button>
                 </section>
+                <section className={styles.whyUsSection}>
+                    <div className={styles.whyUsCard}>
+                        <h3>Lorem ipsum, dasdasd</h3>
+                        <p>
+                            isksodu djskxni sldjskxni s djskxni sdjskxni swsumc
+                            das
+                        </p>
+                    </div>
+                    <div className={styles.whyUsCard}>
+                        <h3>Lorem ipsum, dasdasd</h3>
+                        <p>
+                            isksodu djskxni slwsdjskxni s djskxni sdjskxni sumc
+                            das
+                        </p>
+                    </div>
+                    <div className={styles.whyUsCard}>
+                        <h3>Lorem ipsum, dasdasd</h3>
+                        <p>isksodu djskxni slwsumc djskxni s djskxni sdas</p>
+                    </div>
+                    <div className={styles.whyUsCard}>
+                        <h3>Lorem ipsum, dasdasd</h3>
+                        <p>
+                            isksodu djskxni slwsumc djskxni s djskxni sdjskxni
+                            sdas
+                        </p>
+                    </div>
+                </section>
             </section>
         </>
     );
