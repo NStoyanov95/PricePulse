b33c6b17b3c29c85eff870a338fc92d592416c94
NStoyanov95 <nstoyanov9512@gmail.com>
2024-12-29 17:20:48 +0200

    Add How it works section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -43,6 +43,36 @@ const Home = () => {
                         </p>
                     </div>
                 </section>
+                <section className={styles.howItWorksSection}>
+                    <section className={styles.howItWorksContainer}>
+                        <div className={styles.howItWorksButtons}>
+                            <article>
+                                <h3>Simple shopping ecosystem</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Odio adipisci distinctio
+                                </p>
+                            </article>
+                            <article>
+                                <h3>Simple shopping ecosystem</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Odio adipisci distinctio
+                                </p>
+                            </article>
+                            <article>
+                                <h3>Simple shopping ecosystem</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Odio adipisci distinctio
+                                </p>
+                            </article>
+                        </div>
+                        <div className={styles.howItWorkImage}>
+                            <h2>Here will be the dynamic images..</h2>
+                        </div>
+                    </section>
+                </section>
             </section>
         </>
     );
