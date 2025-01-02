0033d94235dd18ce7fc3315a22cb6ab418ac9854
NStoyanov95 <nstoyanov9512@gmail.com>
2025-01-02 20:30:00 +0200

    Render dynamic images when click on article

--- src/components/Home/HowItWorksSection/HowItWorksSection.jsx
+++ src/components/Home/HowItWorksSection/HowItWorksSection.jsx
@@ -1,25 +1,40 @@
+import { useState } from "react";
 import styles from "./HowItWorksSection.module.css";
 
 const HowItWorksSection = () => {
+    const [currentImage, setCurrentImage] = useState(
+        "https://react-shop-siza.vercel.app/assets/illustration3-Td6aLZcD.svg"
+    );
+
+    const imageUrls = [
+        "https://react-shop-siza.vercel.app/assets/illustration3-Td6aLZcD.svg",
+        "https://react-shop-siza.vercel.app/assets/illustration2-1b2WZaOp.svg",
+        "https://react-shop-siza.vercel.app/assets/illustration1-mvQYH-I0.svg",
+    ];
+
+    const handleArticleClick = (index) => {
+        setCurrentImage(imageUrls[index]);
+    };
+
     return (
         <section className={styles.howItWorksSection}>
             <section className={styles.howItWorksContainer}>
                 <div className={styles.howItWorksButtons}>
-                    <article>
+                    <article onClick={() => handleArticleClick(0)}>
                         <h3>Simple shopping ecosystem</h3>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Odio adipisci distinctio
                         </p>
                     </article>
-                    <article>
+                    <article onClick={() => handleArticleClick(1)}>
                         <h3>Simple shopping ecosystem</h3>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Odio adipisci distinctio
                         </p>
                     </article>
-                    <article>
+                    <article onClick={() => handleArticleClick(2)}>
                         <h3>Simple shopping ecosystem</h3>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing
@@ -28,7 +43,7 @@ const HowItWorksSection = () => {
                     </article>
                 </div>
                 <div className={styles.howItWorkImage}>
-                    <h2>Here will be the dynamic images..</h2>
+                    <img src={currentImage} alt="pic" />
                 </div>
             </section>
         </section>
