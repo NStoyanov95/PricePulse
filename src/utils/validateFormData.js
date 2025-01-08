172df95100dace3fd01c1f8b17d8eec42a88adf5
NStoyanov95 <nstoyanov9512@gmail.com>
2025-01-08 19:37:59 +0200

    Fix formData validator function return message

--- src/utils/validateFormData.js
+++ src/utils/validateFormData.js
@@ -1,12 +1,9 @@
 const validateFormData = (formData) => {
     const newErrors = {};
 
-    if (formData.username.length < 5) {
-        newErrors.username = "Username must be at least 5 characters";
-    }
-
-    if (formData.password.length < 5) {
-        newErrors.password = "Password must be at least 5 characters";
+    if (formData.username.length < 5 || formData.password.length < 5) {
+        newErrors.message =
+            "Username and password must be at least 5 characters";
     }
 
     return newErrors;
